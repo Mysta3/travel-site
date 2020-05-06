afa898faeaac4274c128cc51d33e870ed9ecba75
Mysta3 <myshawnestallings@gmail.com>
2020-05-06 09:42:48 -0700

    added webpack management for css files, added postcss

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,2 +1,2 @@
 import '../styles/styles.css';
-alert('Hello , test!! Cool');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
 
+const postCSSplugins = [
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer'),
+];
+
 module.exports = {
   entry: './app/assets/scripts/App.js',
   output: {
@@ -8,4 +14,16 @@ module.exports = {
   },
   mode: 'development',
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, //only interested in files that end in .css
+        use: [
+          'style-loader',
+          'css-loader?url=false',
+          { loader: 'postcss-loader', options: { plugins: postCSSplugins } },
+        ], //style loader applies css styling, css=loader allows us to use css
+      },
+    ],
+  },
 };
